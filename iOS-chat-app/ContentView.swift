4eb6134c1f514aac9c5c1c2ad4ff60da54581432
Riya Mittal <riyamittal009@gmail.com>
2022-08-11 13:01:20 -0700

    Initial Commit

---
+++ iOS-chat-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-chat-app
+//
+//  Created by Riya Mittal on 8/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-chat-app/iOS_chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_chat_appApp.swift
+//  iOS-chat-app
+//
+//  Created by Riya Mittal on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
